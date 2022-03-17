6de4875a3478261916f9ff0f65754f5120f5a338
flaskymasky <53376718+flaskymasky@users.noreply.github.com>
2022-03-17 18:29:51 +0100

    Add files via upload

---
+++ timer.hpp
@@ -0,0 +1,28 @@
+
+#ifndef TIMER_HEADER
+#define TIMER_HEADER
+
+#include <chrono> // for std::chrono functions
+
+class Timer
+{
+private:
+	// Type aliases to make accessing nested type easier
+	using clock_type = std::chrono::steady_clock;
+	using second_type = std::chrono::duration<double, std::ratio<1> >;
+
+	std::chrono::time_point<clock_type> m_beg { clock_type::now() };
+
+public:
+	void reset()
+	{
+		m_beg = clock_type::now();
+	}
+
+	double elapsed() const
+	{
+		return std::chrono::duration_cast<second_type>(clock_type::now() - m_beg).count();
+	}
+};
+
+#endif
